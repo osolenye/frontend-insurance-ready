0510f0a7dc09d18687fe086c52770b4696939902
osolenye <apasniynegr228@gmail.com>
2024-03-11 22:25:52 +0600

    done redirect from registration

--- js/registration3.js
+++ js/registration3.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    
+
     var noNumberError = document.getElementById("no_number_error");
     noNumberError.style.display = "none";
 
@@ -42,6 +42,32 @@ document.addEventListener("DOMContentLoaded", function () {
             })
             .then((data) => {
                 console.log(data);
+                // fetch("http://212.112.103.137:6457/api/token/", {
+                fetch("https://api.dms.insurance.kg:6458/api/token/", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        username: username,
+                        password: password
+                    })
+                })
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error("Network response is not ok");
+                        }
+                        return response.json();
+                    })
+                    .then(data => {
+                        // console.log(data); // Здесь вы можете обрабатывать полученные данные
+                        localStorage.setItem("accessToken", data.access);
+                        console.log(localStorage.getItem("accessToken"));
+                        window.location.href = "profile.html";
+                    })
+                    .catch(error => {
+                        console.error("There is a problem with the fetch operation", error);
+                    });
             })
             .catch((error) => {
                 console.error("there is a problem with fetch operation", error);
